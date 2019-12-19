9751e425c4919ee5681ff3225ba0c47ee9c650ab
Ian Towey <itowey@overstock.com>
2019-12-19 14:24:06 +0000

    push

---
+++ clojure/cryptopalssolutions/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cryptopalssolutions
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ clojure/cryptopalssolutions/test/cryptopalssolutions/core_test.clj
@@ -0,0 +1,7 @@
+(ns cryptopalssolutions.core-test
+  (:require [clojure.test :refer :all]
+            [cryptopalssolutions.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 1 1))))
